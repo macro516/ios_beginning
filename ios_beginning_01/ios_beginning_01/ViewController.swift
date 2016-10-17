b6a0d3a70b795eb819f71825fd21c67e216428ff
macro516 <macro516@gmail.com>
2016-10-17 16:53:42 +0900

    ios_begnning_01

---
+++ ios_beginning_01/ios_beginning_01/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ios_beginning_01
+//
+//  Created by AhnJeongho on 2016. 10. 17..
+//  Copyright © 2016년 AhnJeongho. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
